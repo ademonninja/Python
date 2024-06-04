903736a9afa5bffb4cffc7933ff9e5b2122cbbfa
Ashlyn Black <howtheduck@gmail.com>
2024-06-04 11:01:32 -0400

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+i = 1
+while i <= 3:
+    print(i)
+    i=i+1
+password = input("Enter Password:")
+if password == "hunter2":
+        print("Okay redditard")
+else:       
+    print("Password is " + password)
